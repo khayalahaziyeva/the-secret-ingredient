a56f4b4d98f8073c8f7fee96cafc8c32752a2ef3
khayalahaziyeva <67167166+khayalahaziyeva@users.noreply.github.com>
2020-12-20 15:32:12 +0400

    Add files via upload

---
+++ javascriptShowMoreorLess.js
@@ -0,0 +1,143 @@
+		function showMoreorLess() {
+  			var points = document.getElementById("points");
+  			var showmore = document.getElementById("showmore");
+  			var showMoreorLessButton = document.getElementById("showMoreorLessButton");
+
+  			if (points.style.display === "none") {
+  				showMoreorLessButton.innerHTML = "Show more";
+    			points.style.display = "inline";
+    			showmore.style.display = "none";
+  			} else {
+  				showMoreorLessButton.innerHTML = "Show less";
+    			points.style.display = "none";
+    			showmore.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess1() {
+			var points1 = document.getElementById("points1");
+  			var showmore1 = document.getElementById("showmore1");
+  			var showMoreorLessButton1 = document.getElementById("showMoreorLessButton1");
+
+  			if (points1.style.display === "none") {
+  				showMoreorLessButton1.innerHTML = "Show more";
+    			points1.style.display = "inline";
+    			showmore1.style.display = "none";
+  			} else {
+  				showMoreorLessButton1.innerHTML = "Show less";
+    			points1.style.display = "none";
+    			showmore1.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess2() {
+			var points2 = document.getElementById("points2");
+  			var showmore2 = document.getElementById("showmore2");
+  			var showMoreorLessButton2 = document.getElementById("showMoreorLessButton2");
+
+  			if (points2.style.display === "none") {
+  				showMoreorLessButton2.innerHTML = "Show more";
+    			points2.style.display = "inline";
+    			showmore2.style.display = "none";
+  			} else {
+  				showMoreorLessButton2.innerHTML = "Show less";
+    			points2.style.display = "none";
+    			showmore2.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess3() {
+			var points3 = document.getElementById("points3");
+  			var showmore3 = document.getElementById("showmore3");
+  			var showMoreorLessButton3 = document.getElementById("showMoreorLessButton3");
+
+  			if (points3.style.display === "none") {
+  				showMoreorLessButton3.innerHTML = "Show more";
+    			points3.style.display = "inline";
+    			showmore3.style.display = "none";
+  			} else {
+  				showMoreorLessButton3.innerHTML = "Show less";
+    			points3.style.display = "none";
+    			showmore3.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess3() {
+			var points3 = document.getElementById("points3");
+  			var showmore3 = document.getElementById("showmore3");
+  			var showMoreorLessButton3 = document.getElementById("showMoreorLessButton3");
+
+  			if (points3.style.display === "none") {
+  				showMoreorLessButton3.innerHTML = "Show more";
+    			points3.style.display = "inline";
+    			showmore3.style.display = "none";
+  			} else {
+  				showMoreorLessButton3.innerHTML = "Show less";
+    			points3.style.display = "none";
+    			showmore3.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess4() {
+			var points3 = document.getElementById("points4");
+  			var showmore3 = document.getElementById("showmore4");
+  			var showMoreorLessButton3 = document.getElementById("showMoreorLessButton4");
+
+  			if (points3.style.display === "none") {
+  				showMoreorLessButton3.innerHTML = "Show more";
+    			points3.style.display = "inline";
+    			showmore3.style.display = "none";
+  			} else {
+  				showMoreorLessButton3.innerHTML = "Show less";
+    			points3.style.display = "none";
+    			showmore3.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess5() {
+			var points3 = document.getElementById("points5");
+  			var showmore3 = document.getElementById("showmore5");
+  			var showMoreorLessButton3 = document.getElementById("showMoreorLessButton5");
+
+  			if (points3.style.display === "none") {
+  				showMoreorLessButton3.innerHTML = "Show more";
+    			points3.style.display = "inline";
+    			showmore3.style.display = "none";
+  			} else {
+  				showMoreorLessButton3.innerHTML = "Show less";
+    			points3.style.display = "none";
+    			showmore3.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess6() {
+			var points3 = document.getElementById("points6");
+  			var showmore3 = document.getElementById("showmore6");
+  			var showMoreorLessButton3 = document.getElementById("showMoreorLessButton6");
+
+  			if (points3.style.display === "none") {
+  				showMoreorLessButton3.innerHTML = "Show more";
+    			points3.style.display = "inline";
+    			showmore3.style.display = "none";
+  			} else {
+  				showMoreorLessButton3.innerHTML = "Show less";
+    			points3.style.display = "none";
+    			showmore3.style.display = "inline";
+  			}
+		}
+
+		function showMoreorLess7() {
+			var points3 = document.getElementById("points7");
+  			var showmore3 = document.getElementById("showmore7");
+  			var showMoreorLessButton3 = document.getElementById("showMoreorLessButton7");
+
+  			if (points3.style.display === "none") {
+  				showMoreorLessButton3.innerHTML = "Show more";
+    			points3.style.display = "inline";
+    			showmore3.style.display = "none";
+  			} else {
+  				showMoreorLessButton3.innerHTML = "Show less";
+    			points3.style.display = "none";
+    			showmore3.style.display = "inline";
+  			}
+		}
